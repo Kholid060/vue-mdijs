41f5d8b2a6b95e1a4256b0599d905b1a0eb20842
Ahmad Kholid <kholid060@gmail.com>
2020-06-07 11:57:31 +0800

    update error message

--- src/components/vue-mdijs.js
+++ src/components/vue-mdijs.js
@@ -28,7 +28,7 @@ export default {
     const icon = this.$options.lib[camelcase(this.name)];
 
     if (typeof icon === 'undefined') {
-      console.error('Name of the icon is not correct')
+      console.error(`[${this.name}] Name of the icon is not correct`);
 
       return;
     }
